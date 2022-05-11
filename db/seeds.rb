25af3e8dd6e12709ca5a3c60ecd54f2481485bae
Fabián A. Hidalgo Neira <fabian.hidalgo@poluxhr.com>
2022-05-11 15:59:32 -0400

    mvc product, seed products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Creating products
+p 'Creating products'
+Product.delete_all if Product.all
+Product.create(
+  [
+    {
+      name: 'Medium Coverage',
+      sell_in: 10,
+      price: 20,
+      created_at: '15/05/2022'
+    },
+    {
+      name: 'Full Coverage',
+      sell_in: 2,
+      price: 0,
+      created_at: '20/05/2022'
+    },
+    {
+      name: 'Low Coverage',
+      sell_in: 5,
+      price: 7,
+      created_at: '1/06/2022'
+    },
+    {
+      name: 'Mega Coverage',
+      sell_in: 0,
+      price: 80,
+      created_at: '15/05/2022'
+    },
+    {
+      name: 'Mega Coverage',
+      sell_in: -1,
+      price: 80
+    },
+    {
+      name: 'Special Full Coverage',
+      sell_in: 15,
+      price: 20
+    },
+    {
+      name: 'Special Full Coverage',
+      sell_in: 10,
+      price: 49,
+      created_at: '15/05/2022'
+    },
+    {
+      name: 'Special Full Coverage',
+      sell_in: 5,
+      price: 49
+    },
+    {
+      name: 'Super Sale',
+      sell_in: 3,
+      price: 6
+    }
+  ]
+)
+p "Product count: #{Product.all.count}"
